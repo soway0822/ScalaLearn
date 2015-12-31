9882964c0a884cf5fe984b80608cc9efa8199697
sowaychang <soway.wayne@gmail.com>
2015-12-31 17:34:30 +0800

    1

--- ScalaLearn/src/ScalaBasic.scala
+++ ScalaLearn/src/ScalaBasic.scala
@@ -7,5 +7,11 @@ object ScalaBasic {
   def main(args: Array[String]): Unit = {
     Logger.getLogger("org").setLevel(Level.OFF)
     System.setProperty("spark.ui.showConsoleProgress", "false")
-  }  
+
+    val conf = new SparkConf().setAppName("ScalaBasic").setMaster("local[4]")
+    val sc = new SparkContext(conf)
+
+    val intRDD = sc.parallelize(List(3, 1, 2, 5, 5))
+    intRDD.collect().foreach(println)
+  }
 }
